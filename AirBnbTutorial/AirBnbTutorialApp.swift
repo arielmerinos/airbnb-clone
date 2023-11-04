80c3da93e334338126403654ca70d2ca621a22f5
arielmerinos <arielmerino@ciencias.unam.mx>
2023-11-04 13:30:51 -0600

    Initial Commit

---
+++ AirBnbTutorial/AirBnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbTutorialApp.swift
+//  AirBnbTutorial
+//
+//  Created by Ariel Merino on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
